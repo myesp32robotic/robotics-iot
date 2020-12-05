58a81a5df59158ebef3c75322f20f89970b9a8ac
myesp32robotic <65606893+myesp32robotic@users.noreply.github.com>
2020-12-05 22:37:13 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {    
+    name : 'MyESP32',
+    platform : 'arduino-esp32',
+    title : 'MyESP32',
+    description : 'MyESP32.\n',
+    author : 'kru thong',
+    website : 'http://www.myrobotstem.com/',
+    email : '.........',
+    git : '......',
+    image : '/static/display.jpg',
+    version : '0.0.1'
+};
